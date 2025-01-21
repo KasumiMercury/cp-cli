e651894e642670af96402910e4eb6974bad7c3b5
KasumiMercury <naoki_shira@outlook.jp>
2025-01-21 13:50:33 +0900

    refactor: impl

--- cmd/create.go
+++ cmd/create.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -8,37 +5,6 @@ import (
 )
 
 var createCmd = create.NewCmd()
-
-// createCmd represents the create command.
-//var createCmd = &cobra.Command{
-//	Use:   "create",
-//	Short: "Create a new Go project directory",
-//	Long: `Initialize a new Go project with a standardized directory structure designed for competitive programming.
-//Creates essential files and folders including main solution file, test cases directory, and template code.
-//
-//Sets up a complete development environment ready for implementing and testing algorithmic solutions.`,
-//	Run: func(_ *cobra.Command, _ []string) {
-//		pID, err := create.ParseIDFromURL(targetURL)
-//		if err != nil {
-//			slog.Error(err.Error())
-//		}
-//
-//		fmt.Println(pID)
-//
-//		if err := create.ProjectDir(pID); err != nil {
-//			slog.Error(err.Error())
-//		}
-//
-//		genPath, err := create.ProjectMain(pID)
-//		if err != nil {
-//			slog.Error(err.Error())
-//		}
-//
-//		if err := edit.OpenEditor(genPath); err != nil {
-//			slog.Error(err.Error())
-//		}
-//	},
-//}
 
 func init() {
 	rootCmd.AddCommand(createCmd)
--- create/command.go
+++ create/command.go
@@ -1,6 +1,8 @@
 package create
 
 import (
+	"fmt"
+	"github.com/KasumiMercury/cp-cli/edit"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +15,33 @@ Creates essential files and folders including main solution file, test cases dir
 
 Sets up a complete development environment ready for implementing and testing algorithmic solutions.`,
 		RunE: func(cmd *cobra.Command, _ []string) error {
-			// TODO: implement
+			targetURL, err := cmd.Flags().GetString("target")
+			if err != nil {
+				return fmt.Errorf("failed to get target URL: %w", err)
+			}
+
+			cmd.Printf("Target URL: %s\n", targetURL)
+
+			pID, err := ParseIDFromURL(targetURL)
+			if err != nil {
+				return fmt.Errorf("failed to parse target URL: %w", err)
+			}
+
+			cmd.Printf("detected Project ID: %s\n", pID)
+
+			if err := ProjectDir(pID); err != nil {
+				return fmt.Errorf("failed to create project directory: %w", err)
+			}
+
+			genPath, err := ProjectMain(pID)
+			if err != nil {
+				return fmt.Errorf("failed to create main.go: %w", err)
+			}
+
+			if err := edit.OpenEditor(genPath); err != nil {
+				return fmt.Errorf("failed to open editor: %w", err)
+			}
+
 			return nil
 		},
 	}
