6912ac96d947d1bf8ece723bff35f4c536d39f31
KasumiMercury <naoki_shira@outlook.jp>
2025-01-24 22:27:56 +0900

    test: create project

---
+++ create/project_test.go
@@ -0,0 +1,33 @@
+package create
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestProjectDir(t *testing.T) {
+	tempDir := t.TempDir()
+	projectDir := filepath.Join(tempDir, "temp")
+	problemID := "example_id"
+
+	if err := ProjectDir(projectDir, problemID); err != nil {
+		t.Errorf("ProjectDir() error = %v", err)
+	}
+
+	if _, err := os.Stat(projectDir); os.IsNotExist(err) {
+		t.Errorf("ProjectDir() error = %v", err)
+	}
+}
+
+func Test_projectMain(t *testing.T) {
+	tempDir := t.TempDir()
+
+	if err := projectMain(tempDir); err != nil {
+		t.Errorf("projectMain() error = %v", err)
+	}
+
+	if _, err := os.Stat(tempDir); os.IsNotExist(err) {
+		t.Errorf("projectMain() error = %v", err)
+	}
+}
