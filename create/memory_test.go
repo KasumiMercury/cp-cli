cd87afe892bd5372f79849723eaf5b8a951927ff
KasumiMercury <naoki_shira@outlook.jp>
2025-01-24 22:47:28 +0900

    test: memory current project

---
+++ create/memory_test.go
@@ -0,0 +1,36 @@
+package create
+
+import (
+	"github.com/spf13/viper"
+	"os"
+	"path"
+	"reflect"
+	"testing"
+)
+
+func TestMemoryCurrentProject(t *testing.T) {
+	tempDir := t.TempDir()
+	viper.AddConfigPath(tempDir)
+	viper.SetConfigType("yaml")
+	viper.SetConfigName(".cp-cli")
+
+	_, _ = os.Create(path.Join(tempDir, ".cp-cli.yaml"))
+
+	tempProjectID := "example_id"
+	tempProjectSite := "example.com"
+
+	if err := MemoryCurrentProject(tempProjectID, tempProjectSite); err != nil {
+		t.Errorf("failed to open project %v", err)
+	}
+
+	wantCurrent := map[string]string{
+		"id":   tempProjectID,
+		"site": tempProjectSite,
+	}
+
+	actualCurrentProject := viper.Get("current_project")
+	if !reflect.DeepEqual(actualCurrentProject, wantCurrent) {
+		t.Errorf("current_project = %v, want %v", actualCurrentProject, wantCurrent)
+	}
+
+}
