482100b33b7bf52af6f693940e23fa6623cb680a
KasumiMercury <naoki_shira@outlook.jp>
2025-02-05 22:45:18 +0900

    feat: set config

--- config/set/set.go
+++ config/set/set.go
@@ -1,6 +1,10 @@
 package set
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/KasumiMercury/cp-cli/config/option"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
 
 func NewCmd() *cobra.Command {
 	setCmd := &cobra.Command{
@@ -16,4 +20,23 @@ func NewCmd() *cobra.Command {
 	return setCmd
 }
 
-func set(cmd *cobra.Command, args []string) {}
+func set(cmd *cobra.Command, args []string) {
+	if !validOption(args[0]) {
+		return
+	}
+
+	viper.Set(args[0], args[1])
+	err := viper.WriteConfig()
+	if err != nil {
+		return
+	}
+}
+
+func validOption(key string) bool {
+	options := option.Options
+	if _, ok := options[option.OptionKey(key)]; !ok {
+		return false
+	}
+
+	return true
+}
