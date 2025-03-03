c4b6a0d6adf311340db67e49a41035e98544a38c
KasumiMercury <naoki_shira@outlook.jp>
2025-03-03 19:45:08 +0900

    feat: remove current-project config
    prevent inconsistencies caused by manual settings

--- config/option/option.go
+++ config/option/option.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/spf13/viper"
 	"os"
-	"strings"
 )
 
 type Key string
@@ -141,60 +140,4 @@ func (r *Registry) Get(key string) (Option, error) {
 
 func (r *Registry) GetAll() map[string]Option {
 	return r.options
-}
-
-type Item struct {
-	String   func() string
-	Validate func(string) error
-}
-
-var Options = map[Key]Item{
-	EditorKey: {
-		String: func() string {
-			v := viper.GetString("editor")
-			if v == "" {
-				return NoConfigMessage
-			}
-
-			return v
-		},
-	},
-	WorkspaceKey: {
-		String: func() string {
-			v := viper.GetString("project_dir")
-			if v == "" {
-				return NoConfigMessage
-			}
-
-			return v
-		},
-		Validate: func(v string) error {
-			if f, err := os.Stat(v); os.IsNotExist(err) || !f.IsDir() {
-				return ErrNotDirectory
-			}
-
-			return nil
-		},
-	},
-	CurrentProjectKey: {
-		String: func() string {
-			project := viper.GetStringMapString("current_project")
-			if project == nil {
-				return NoConfigMessage
-			}
-
-			strBuilder := strings.Builder{}
-			strBuilder.WriteString("\n")
-
-			strBuilder.WriteString("\t")
-			strBuilder.WriteString("id: ")
-			strBuilder.WriteString(project["id"])
-			strBuilder.WriteString("\n")
-			strBuilder.WriteString("\t")
-			strBuilder.WriteString("site: ")
-			strBuilder.WriteString(project["site"])
-
-			return strBuilder.String()
-		},
-	},
 }
